cc96f37092a1ce307f88f711e60a6b1d03a95518
AdarshKhatri01 <24mt1102@iiitu.ac.in>
2025-11-06 00:58:20 +0530

    Split 30/70, Created File Upload Button

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,12 @@
+import FileUpload from "@/components/FileUpload";
 
 export default function Home() {
   return (
     <div>
       <div className="min-h-screen w-screen grid grid-cols-10 gap-4 p-4 font-bold">
-        <div className="bg-pink-500 col-span-3">1</div>
+        <div className="bg-pink-500 col-span-3 flex items-center justify-center">
+          <FileUpload />
+        </div>
         <div className="bg-pink-500 col-span-7">2</div>
       </div>
     </div>
--- src/components/fileUpload.jsx
+++ src/components/fileUpload.jsx
@@ -1,12 +1,29 @@
 "use client"
-import React from 'react'
+import React from 'react';
+import { Upload } from 'lucide-react';
 
-const fileUpload = () => {
+const FileUpload = () => {
+
+    const handleFileUploadClick = () => {
+        const ele = document.createElement('input');
+        ele.setAttribute("type", "file");
+        ele.setAttribute("accept", "application/pdf");
+        ele.click();
+        ele.addEventListener("change", (e) => {
+            if (ele.files && ele.files.length > 0) {
+                const file = ele.files[0];
+            }
+        })
+    }
+
     return (
-        <div>
-
+        <div className='bg-slate-400 border-2 rounded-lg p-4 cursor-pointer' onClick={handleFileUploadClick}>
+            <div className='flex flex-col items-center justify-center' >
+                <h3>Upload Pdf here...</h3>
+                <Upload />
+            </div>
         </div>
     )
 }
 
-export default fileUpload
+export default FileUpload
